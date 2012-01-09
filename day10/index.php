96b413591b8aa4f22acb4f8399e7cfffbaa1f64e
Miguel <Miguel@Miguel.(none)>
2012-01-09 18:37:54 -0300

    practicas day 10

---
+++ day10/index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+     <meta charset="utf-8">
+    <head>
+        <meta charset="utf-8">
+        <title>CSS playground</title>
+        <link rel="STYLESHEET" type="text/css" href="css/style.css">
+    </head>
+
+    <body>
+        <header class="topHeader">
+            <div id="logo">
+                <img src="http://media.tumblr.com/tumblr_lqhtkhKcgF1qfmfxq.png" />
+            </div>
+            <h1>CSS Playground</h1>
+        </header>
+        <nav>
+            <ul id="menu" >
+                <li ><a id="home" href="">Home</a></li>
+                <li ><a id="dia1" href="">Day 1</a></li>
+                <li ><a id="dia2"  href="">Day 2</a></li>
+                <li ><a id="dia3" href="">Day 3</a></li>
+ 
+            </ul>
+        </nav>
+
+        <section id="seccion">
+            <article >
+                <header>
+                    <h2 id="firstTitle" class="test">Maecenas ac Nisi</h2>
+                    <p></p>
+                    <p>
+                        Posted on <time datetime="2009-09-04T16:31:24+02:00">September 4th 2009</time> 
+                        by <a href="#" class="href1">Writer</a> - <a href="#comments" class="href2" >6 comments</a>
+                    </p>
+                </header>
+               
+                <p id="column">
+                    Maecenas ac nisi vel <span class="hiOne">nunc</span> rhoncus lacinia sed nec felis. Aenean nec urna purus. <span class="hiTwo">Nam</span> ut mauris eros, nec ullamcorper sem. Aenean vitae nunc sit amet sem molestie ultricies. Ut eu nisi augue. Nullam eros sem, blandit quis sagittis in, pharetra in enim. Donec fringilla, lorem sit amet feugiat eleifend, lectus arcu lobortis turpis, sed bibendum risus tellus nec justo. Duis dignissim est sagittis elit accumsan id viverra nisl ornare. Aenean et sapien quis nisl malesuada sagittis facilisis vitae purus. Nullam non nulla at libero convallis ultricies. Sed semper, nisl vitae condimentum rutrum, sapien leo placerat nulla, quis lobortis ligula velit sed elit.
+
+                    <span class="lowOne">Nam</span> nec sapien ut ante congue <span class="hiOne">vulputate</span>. Donec fringilla euismod tortor, non viverra erat <span class="hiTwo">sollicitudin</span> eu. Morbi sit amet lobortis tellus. Curabitur vel velit sem, in ultricies orci. Morbi congue rhoncus velit, <span class="hiThree">fringilla</span> aliquam felis blandit nec. Morbi molestie, turpis nec posuere interdum, mauris orci semper lorem, ac mollis tellus mauris commodo metus. Nullam bibendum euismod lectus sed volutpat. 
+                </p>
+                <video width="400" height="300" controls="controls">
+                    <source src="http://people.xiph.org/~maikmerten/demos/BigBuckBunny.ogv" />
+                    Your browser does not support the video tag.
+                </video> 
+            </article>
+
+            <article >
+                <header>
+                    <h2>Lorem Ipsum</h2>
+                    <p></p>
+                    <p>
+                        Posted on <time datetime="2009-09-04T16:31:24+02:00">September 4th 2009</time> 
+                        by <a href="#" class="href1">Writer</a> - <a href="#comments" class="href2">6 comments</a>
+                    </p>
+                    
+                </header>
+                <p id="column">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse non est quis nunc aliquam interdum. <span class="lowOne">Quisque</span> sapien sem, adipiscing at porttitor eget, <span class="hiTwo">pretium</span> id ipsum. Aenean lacinia, enim nec semper vestibulum, <span class="hiThree">purus</span> tellus elementum nunc, vitae rutrum nibh purus quis odio. Nunc enim lorem, hendrerit sed porta in, aliquam quis enim. Sed ac tortor elit. Duis vehicula aliquam odio, nec facilisis augue pharetra a. In mauris metus, gravida ac pulvinar quis, convallis fringilla lectus. Aliquam erat volutpat. Pellentesque ornare eleifend est, eget fermentum sapien rutrum eget. Mauris cursus eleifend arcu, blandit euismod velit facilisis sit amet. Nulla facilisi. Sed ligula tortor, mollis eget bibendum consectetur, dignissim ut orci. Phasellus eu velit non ante tempor condimentum. Etiam vestibulum scelerisque eleifend. Nunc quam nunc, consectetur at pellentesque id, pellentesque vel purus.
+                    Mauris at odio at tellus ornare fringilla vitae id justo. Mauris <span class="hiOne">laoreet</span>, nunc non venenatis fermentum, ante ligula ultrices lacus, ac aliquet nunc magna ac dui. Integer placerat pellentesque euismod. Sed tempus ullamcorper ligula et ornare. Suspendisse lobortis lobortis eleifend. Curabitur pharetra, metus in venenatis fermentum, urna leo iaculis augue, sit amet congue nisi eros in velit. Morbi iaculis dignissim auctor. Proin vehicula eros et ante mattis a sollicitudin metus malesuada. Morbi sit amet pellentesque felis. Nulla sem tellus, viverra at congue vel, placerat eu mi. Aliquam lacus lectus, euismod in ornare id, molestie ut elit.
+
+                    <span class="lowOne">Praesent</span> semper massa sed dolor tincidunt id <span class="lowOne">facilisis</span> quam elementum. Aliquam sed ipsum dui, eu luctus est. Nullam felis metus, varius nec <span class="hiOne">interdum</span> sed, varius vel mi. Nunc non <span class="hiTwo">lectus</span> eget orci egestas scelerisque. Aliquam erat volutpat. Phasellus at metus nunc. Nam suscipit vulputate diam. Vivamus eu metus et leo aliquet consectetur. Maecenas interdum, mauris vitae adipiscing tristique, ligula libero bibendum nisi, eget sodales dolor tellus accumsan quam. Suspendisse lacus odio, volutpat et accumsan ac, sollicitudin vestibulum erat. Morbi erat diam, aliquet vitae fermentum in, blandit sed nulla. Aenean venenatis lectus in augue varius fermentum. Curabitur mauris est, tristique et venenatis in, volutpat in enim.
+                </p>
+                 
+            </article>
+            
+ 
+            
+              <aside>
+                <h2>About section</h2>
+                <p >Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+            	</aside>
+            
+        </section>
+        
+          
+
+        <footer>
+            <p>&copy; Copyright 2011 - HTML5 Team</p>
+        </footer>
+    </body>
+</html>
