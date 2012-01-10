f1877d5e6ab8892881c97aed8a8d3b48684a211e
Miguel <Miguel@Miguel.(none)>
2012-01-10 02:19:18 -0300

    practicas 1,2 y 3 del day 11

--- day11/script/script.js
+++ day11/script/script.js
@@ -1,6 +1,25 @@
 function disp(){
       	var data = $( 'textarea' ).val();
-      	$('#log').append('<script>'+data+'</script>');
-      	
-      	
+      	var save = $( 'input:checkbox' ).val();
+      	if (save){
+      			window.localStorage.setItem('value', "<script>"+data+"</script>");
+	        	window.localStorage.setItem('timestamp', (new Date()).getTime());
+	        	alert("data guardada");
+      	}
+      	$('#log').append('<script>'+data+'</script>');    	
+}
+function dragAndDrop(){
+	document.querySelector('#dropzone').addEventListener('drop', function(e) {
+	e.dataTransfer.effectAllowed = e.target.className;
+  e.target.style.border = "4px solid #cc3300";
+  var reader = new FileReader();
+  reader.onload = function(evt) {
+    document.querySelector('img').src = evt.target.result;
+  };
+
+  reader.readAsDataURL(e.dataTransfer.files[0]);
+}, false);
+}
+function drop(){
+		alert('dropearon algo');
 }
